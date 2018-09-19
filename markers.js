0892ba89c94ef21621408822bc4cb90564811e83
alexsyeo <alexsyeo@comcast.net>
2018-09-19 11:29:05 -0700

    first commit

---
+++ markers.js
@@ -0,0 +1,26 @@
+var waterFountainMarkers = [
+    {
+        coords:{lat:37.870557, lng:-122.260546},
+        content:'Dwinelle Hall'
+    },
+    {
+        coords:{lat:37.871109, lng:-122.259293},
+        content:'Wheeler Hall'
+    },
+    {
+        coords:{lat:37.875653, lng:-122.258522},
+        content:'Soda Hall'
+    },
+    {
+        coords:{lat:37.869148, lng:-122.259536},
+        content:'ASUC Student Union, Second Floor'
+    },
+    {
+        coords:{lat:37.871278, lng:-122.254668},
+        content:'Minor Hall'
+    },
+    {
+        coords:{lat:37.870110, lng:-122.257778},
+        content:'Barrows Hall'
+    }
+];
