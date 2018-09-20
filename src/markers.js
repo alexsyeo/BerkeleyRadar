4b7d4cf95d1566d3a216c99ec1774ab35f3225c7
alexsyeo <alexsyeo@comcast.net>
2018-09-19 20:23:09 -0700

    updates

--- src/markers.js
+++ src/markers.js
@@ -1,26 +1,65 @@
 var waterFountainMarkers = [
     {
         coords:{lat:37.870557, lng:-122.260546},
-        content:'Dwinelle Hall'
+        content:'Dwinelle Hall',
+        title:'Dwinelle Hall'
     },
     {
         coords:{lat:37.871109, lng:-122.259293},
-        content:'Wheeler Hall'
+        content:'Wheeler Hall',
+        title:'Wheeler Hall'
     },
     {
         coords:{lat:37.875653, lng:-122.258522},
-        content:'Soda Hall'
+        content:'Piece of crap',
+        title:'Soda Hall'
     },
     {
         coords:{lat:37.869148, lng:-122.259536},
-        content:'ASUC Student Union, Second Floor'
+        content:'ASUC Student Union, Second Floor',
+        title:'ASUC Student Union, Second Floor'
     },
     {
         coords:{lat:37.871278, lng:-122.254668},
-        content:'Minor Hall'
+        content:'Minor Hall',
+        title:'Minor Hall'
     },
     {
         coords:{lat:37.870110, lng:-122.257778},
-        content:'Barrows Hall'
+        content:'Barrows Hall',
+        title:'Barrows Hall'
+    }
+];
+
+var bathroomMarkers = [
+    {
+        coords:{lat:37.868707, lng:-122.262575},
+        content:'RSF Locker Rooms',
+        title:'RSF Locker Rooms'
+    },
+    {
+        coords:{lat:37.871109, lng:-122.259293},
+        content:'Wheeler Hall',
+        title:'Wheeler Hall'
+    },
+    {
+        coords:{lat:37.875653, lng:-122.258522},
+        content:'Soda Hall',
+        title:'Soda Hall'
+    },
+    {
+        coords:{lat:37.869148, lng:-122.259536},
+        content:'ASUC Student Union, Second Floor',
+        title:'ASUC Student Union, Second Floor'
+    },
+    {
+        coords:{lat:37.872091, lng:-122.259011},
+        content:'Doe Memorial Library',
+        title:'Doe Memorial Library'
+    },
+    {
+        coords:{lat:37.871074, lng:-122.255669},
+        content:'Hertz Hall',
+        title:'Hertz Hall'
     }
 ];
